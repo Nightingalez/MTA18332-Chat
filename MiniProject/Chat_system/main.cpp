12e4a5fbf8432ef8105595a6f582606fe9abd1b2
Victor <vroua17@student.aau.dk>
2018-12-03 12:58:33 +0100

    Updated the main C++ file of the project
    Added two more libraries (one for vectors and the other for sockets) as well as a template of a data structure block

--- MiniProject/Chat_system/main.cpp
+++ MiniProject/Chat_system/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // library used for input and output
 #include <string.h> // library used for string variables
 #include <stdio.h> // library used for void, char, and int functions
+#include <vector> // library used for vectors
+#include <winsock2.h> // library used for sockets
 
 using namespace std;
 
@@ -8,3 +10,7 @@ int main() // main function used for the server
 {
 
 }
+
+struct structure{
+
+} // data structure block
