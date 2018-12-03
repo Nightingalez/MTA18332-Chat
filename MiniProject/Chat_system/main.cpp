ead46159b4c107d14d59e4175d5794d3dd2143e5
Victor <vroua17@student.aau.dk>
2018-12-03 12:45:52 +0100

    Updated the library section in the beginning of the code

--- MiniProject/Chat_system/main.cpp
+++ MiniProject/Chat_system/main.cpp
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stlib.h>
+#include <iostream> // library used for input and output
+#include <string.h> // library used for string variables
+#include <stdio.h> // library used for void, char, and int functions
 
 using namespace std;
 
-int main()
+int main() // main function used for the server
 {
 
 }
